c0913097629811c4d1b36be147eb0bda074c3042
atulmaps <atul.k.srivastav@oracle.com>
2022-08-13 23:59:04 +0530

    Best Time to Buy and Sell Stock

--- geek75/src/Application.java
+++ geek75/src/Application.java
@@ -3,10 +3,20 @@ import arrays.ArrayProblems;
 public class Application {
 
     public static void main(String[] args) {
-        int[] input = {2,7,11,15};
-        int target = 9;
-        int[] output = ArrayProblems.twoSum(input, target);
-        System.out.println("indices of the two numbers such that they add up to target: " +
-                "["  + output[0] + ", " + output[1] + "]");
+
+//        // Two sum
+//        int[] input = {2,7,11,15};
+//        int target = 9;
+//        int[] output = ArrayProblems.twoSum(input, target);
+//        System.out.println("indices of the two numbers such that they add up to target: " +
+//                "["  + output[0] + ", " + output[1] + "]");
+
+        // Best Time to Buy and Sell Stock
+        int[] prices = {7,1,5,3,6,4}; //{7,6,4,3,1};
+        int maxProfit = ArrayProblems.maxProfit(prices);
+        System.out.println("Maximum profit : " + maxProfit);
+
+
+
     }
 }
--- geek75/src/arrays/ArrayProblems.java
+++ geek75/src/arrays/ArrayProblems.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 public class ArrayProblems {
 
+    // Two sum
     /*
     Given an array of integers nums and an integer target,
     return indices of the two numbers such that they add up to target.
@@ -31,4 +32,41 @@ public class ArrayProblems {
         }
         return new int[] {-1, -1};
     }
+
+    // Best Time to Buy and Sell Stock
+    /*
+    * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+    * You want to maximize your profit by choosing a single day to buy one stock and
+    * choosing a different day in the future to sell that stock.
+    *
+    * Return the maximum profit you can achieve from this transaction.
+    * If you cannot achieve any profit, return 0.
+    *
+    * */
+
+    /*
+    Input: prices = [7,1,5,3,6,4]
+    Output: 5
+    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+    Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+    * */
+
+    public static int maxProfit(int[] prices) {
+
+        int minBuyingPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int i=1; i < prices.length; i++) {
+            int currPrice = prices[i];
+            if(currPrice > minBuyingPrice) {
+                maxProfit = Math.max(maxProfit, currPrice - minBuyingPrice);
+            }
+            else {
+                minBuyingPrice = currPrice;
+                maxProfit = 0;
+            }
+        }
+        return maxProfit;
+    }
+
 }
